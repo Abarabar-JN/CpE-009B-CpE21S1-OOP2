a061d03ba5b4e621a5c9bb1323a4a75d242ba5e7
Abarabar-JN <qjnrabarabar@tip.edu.ph>
2024-11-07 12:36:07 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
-import tkinter as tk
-from registration import RegistrationForm
-
 def main():
-    root = tk.Tk()
-    app = RegistrationForm(root)
-    root.mainloop()
+ class TemperatureConversion:
+  def __init__(self, temp=1):
+   self._temp = temp
+ class CelsiusToFahrenheit(TemperatureConversion):
+  def conversion(self):
+   return (self._temp * 9) / 5 + 32
+ class CelsiusToKelvin(TemperatureConversion):
+  def conversion(self):
+   return self._temp + 273.15
+ class FahrenheitToCelsius(TemperatureConversion):
+  def conversion(self):
+   return (self._temp - 32) * 5/9
+ class FahrenheitToKelvin(TemperatureConversion):
+  def conversion(self):
+   return (self._temp - 32) * 5/9 + 273.15
+ class KelvinToCelsius(TemperatureConversion):
+  def conversion(self):
+   return self._temp - 273.15
+ class KelvintoFahrenheit(TemperatureConversion):
+  def conversion(self):
+   return (self._temp - 273.15) * 9/5 + 32
 
-if __name__ == "__main__":
-    main()
+ tempInCelsius = float(input("Enter the temperature in Celsius: "))
+ convert = CelsiusToKelvin(tempInCelsius)
+ print(str(convert.conversion()) + " Celsius to Kelvin")
+ convert = CelsiusToFahrenheit(tempInCelsius)
+ print(str(convert.conversion()) + " Celsius to Fahrenheit")
+ tempInFahrenheit = float(input("Enter the temperature in Fahrenheit: "))
+ convert = FahrenheitToCelsius(tempInFahrenheit)
+ print(str(convert.conversion()) + " Fahrenheit to Celsius")
+ convert = FahrenheitToKelvin(tempInFahrenheit)
+ print(str(convert.conversion()) + " Fahrenheit to Kelvin")
+ tempInKelvin = float(input("Enter the temperature in Kelvin: "))
+ convert = KelvinToCelsius(tempInKelvin)
+ print(str(convert.conversion()) + " Kelvin to Celsius")
+ convert = KelvintoFahrenheit(tempInKelvin)
+ print(str(convert.conversion()) + " Kelvin to Fahrenheit")
 
+
+main()
